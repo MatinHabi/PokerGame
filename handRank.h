194c3d944a7ea3c5754bb69fe8cb50f0dd692c44
MatinHabi <a1888637@adelaide.edu.au>
2026-02-07 00:14:32 +1030

    logic

--- handRank.h
+++ handRank.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <vector>
+#include <unordered_set>
 #include "card.h"
+using namespace std;
 
 enum class Rating{
+    Nothing = -1,
     HighCard = 0,
     OnePair = 1,
     TwoPair = 2,
@@ -11,37 +14,55 @@ enum class Rating{
     Flush = 5,
     FullHouse = 6,
     FourOfAKind = 7,
-    StraightFlush = 8
-};
-
-struct HandValue{
-    Rating rank;
-    std::vector<int> tieBreaker; //used to break ties between hands of the same rank, ordered by importance
+    StraightFlush = 8,
+    RoyalFlush = 9,
 };
 
 class HandRank{
 public:
-    static HandValue evaluateHand(std::vector<Cards>& hand, std::vector<Cards>& community){
+    static Rating evaluateHand(std::vector<Cards>& hand, std::vector<Cards>& community){
         //TODO: implement hand evaluation logic
         //consider all options C(7,5) for best 5-card hand from 7 cards
         //cannot use less than 1 card from player hand
 
-        std::unordered_map<Cards, int> h, c;
-        int idx = 0;
-        for(auto i : hand){h[i] = idx++;}
-        for(auto i : community){c[i] = idx++;}
+        int rankCount[15] = {0};
+        int suitCount[5] = {0};
 
-        //royal flush
-        Suit s; int found = 5;
-        std::string rf = "AKQJT";
-        for(char i : rf){
-            Cards temp = {}
-            if(c.find(i) == c.end()){
-
-            }
+        for(auto h: hand){
+            rankCount[h.rank]++;
+            suitCount[h.suit]++;
         }
 
+        for(auto c: community){
+            rankCount[c.rank]++;
+            suitCount[c.suit]++;
+        }
 
+        Rating result = Rating::Nothing;
+        bool tPair = false, rf = false;
+
+        bool hasPair = false;
+        bool hasTwoPair = false;
+        bool hasTrips = false;
+        bool hasQuads = false;
+        bool hasStraight = false;
+        bool hasFlush = false;
+        bool hasStraightFlush = false;
+        bool hasRoyalFlush = false;
+
+        int pairCount = 0;
+
+        for(int i = 2 ; i <= 14 ; i++){
+            if(rankCount[i] == 4){hasQuads = true;}
+            if(rankCount[i] == 3){hasTrips = true;}
+            if(rankCount[i] == 2){pairCount++;}
+        }
+
+        hasPair = pairCount >= 1;
+        hasTwoPair = pairCount >= 2;
+
+        //straight detection
+        
     }
 };
 
